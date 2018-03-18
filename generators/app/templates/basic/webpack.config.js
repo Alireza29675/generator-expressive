6fb0289d34cc6132328fa73cdbcbb18b866a187d
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-03-18 16:58:00 +0330

    webpack config can be customize if user wants react or sass

--- generators/app/templates/basic/webpack.config.js
+++ generators/app/templates/basic/webpack.config.js
@@ -7,14 +7,15 @@ module.exports = {
         sourceMapFilename: './javascripts/bundle.js.map'
     },
     module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader',
-            query: { presets: ['es2015'] }
-        }, {
-            test: /\.sass$/,
-            use: [{ loader: "style-loader" }, { loader: "css-loader?-url" }, { loader: "sass-loader" }]
-        }]
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: { presets: ['es2015'<%if (withReact) {%>, 'react'<%}%>] }
+            }<% if (withSass) { %>, {
+                test: /\.sass$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader?-url" }, { loader: "sass-loader" }]
+            }<% } %>]
     }
 };
