fe73d48c388f7de2dad3e31cf493d1bd940db271
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-03-20 23:22:23 +0330

    Converting all constants to const

--- generators/app/templates/basic/app.js
+++ generators/app/templates/basic/app.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+const index = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
